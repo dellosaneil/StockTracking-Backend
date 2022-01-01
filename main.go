abc5f624ebc4cedc0ce93ef1c8adfadb895c69b7
dellosaneilquipper <dellosaneil@gmail.com>
2022-01-01 17:01:00 +0800

    connect to mysql database

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dellosaneil/stocktracking-backend/db"
 	"github.com/dellosaneil/stocktracking-backend/handlers"
 	"github.com/gorilla/mux"
 )
+
+func init() {
+	db.ConnectDatabase()
+	defer db.StockTrackingDb.Close()
+}
 
 func main() {
 	r := mux.NewRouter()
